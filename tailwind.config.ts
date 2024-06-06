e2ae9b462da2a13f6c0843e4d3a65b51a1956299
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-06 12:49:59 +0530

    github star button added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,10 +70,16 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        gradient: {
+          to: {
+            backgroundPosition: "var(--bg-size) 0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        gradient: "gradient 8s linear infinite",
       },
     },
   },
