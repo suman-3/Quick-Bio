a47a526574ce6ba66f291ee29b0c858a25ba1ebf
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-24 19:38:30 +0530

    minimal style fix and home button added to saved page

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -3,10 +3,11 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Link from "next/link";
 import React from "react";
+import { Home } from "lucide-react";
 
 const page = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center py-10 gap-3 md:gap-4 lg:gap-5">
+    <div className="w-full h-full flex flex-col items-center py-10 gap-3 md:gap-4 lg:gap-5 relative">
       <div className="">
         <h1 className="font-extrabold text-2xl md:text-3xl slg:text-4xl lg:text-6xl text-center w-full lg:w-[90%] uppercase mx-auto pt-4 pb-2 select-none pointer-events-none">
           BIO COLLECTION
@@ -18,6 +19,15 @@ const page = () => {
           <BioCard />
         </ScrollArea>
       </div>
+      <Link href="/">
+        <Button
+          className="absolute top-5 left-5 md:top-7 md:left-7   lg:top-10 lg:left-12 flex items-center gap-1"
+          variant="secondary"
+        >
+          <Home className="size-4 shrink-0" />
+          <span className="hidden md:block">Home</span>
+        </Button>
+      </Link>
     </div>
   );
 };
