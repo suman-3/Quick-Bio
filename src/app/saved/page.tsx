e155934400d76fe90de8dad916200c2d96f92729
Suman Mondal <mondalsumanrock20@gmail.com>
2024-06-24 20:13:38 +0530

    when there is no saved bio then , show the empty card

--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -1,11 +1,23 @@
+"use client";
 import BioCard from "@/components/bioCollection/BioCard";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Home } from "lucide-react";
+import { Bio, getBio } from "@/actions/get-bio";
 
 const page = () => {
+  const [bios, setBios] = useState<Bio[]>([]);
+
+  useEffect(() => {
+    const fetchBios = async () => {
+      const data = await getBio();
+      setBios(data.bios);
+    };
+    fetchBios();
+  }, []);
+
   return (
     <div className="w-full h-full flex flex-col items-center py-10 gap-3 md:gap-4 lg:gap-5 relative">
       <div className="">
@@ -15,9 +27,19 @@ const page = () => {
       </div>
 
       <div className="h-full w-full px-4 md:px-6 lg:px-10">
-        <ScrollArea className="flex min-h-[60vh] max-h-[84vh] mt-2 flex-col rounded-lg bg-muted/50 backdrop-blur-sm overflow-hidden border border-gray-500/50 px-2 md:px-4 lg:px-5 py-3 lg:py-5">
-          <BioCard />
-        </ScrollArea>
+        {bios.length > 0 ? (
+          <ScrollArea className="flex min-h-[60vh] max-h-[84vh] mt-2 flex-col rounded-lg bg-muted/50 backdrop-blur-sm overflow-hidden border border-gray-500/50 px-2 md:px-4 lg:px-5 py-3 lg:py-5">
+            <BioCard />
+          </ScrollArea>
+        ) : (
+          <div className="flex min-h-[60vh] max-h-[84vh] items-center justify-center mt-2 rounded-lg bg-muted/50 backdrop-blur-sm overflow-hidden border border-gray-500/50">
+            <Link href="/" className="flex flex-col space-y-2">
+              <span className="text-xl md:text-2xl">
+                Bio Collection is Empty
+              </span>
+            </Link>
+          </div>
+        )}
       </div>
       <Link href="/">
         <Button
